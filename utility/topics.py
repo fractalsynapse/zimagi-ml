e9c19c71f50dac276d5468563b5d677a8c049929
Adrian Webb <adrian@webb.sh>
2024-04-04 15:26:44 -0400

    Adding an initial topic modelling utility class.

---
+++ utility/topics.py
@@ -0,0 +1,49 @@
+from spacy.lang.en import stop_words
+
+import spacy
+
+
+class TopicModel(object):
+
+    def __init__(self):
+        self.spacy = spacy.load('en_core_web_lg')
+
+
+    def filtered_index(self, full_texts, context_texts):
+        context_index = self.get_index(*context_texts)
+        index = {}
+
+        for topic, count in self.get_index(*full_texts).items():
+            for context_topic in context_index.keys():
+                if set(topic.split()).intersection(context_topic.split()):
+                    index[topic] = count
+
+        return index
+
+
+    def get_index(self, *texts):
+        index = {}
+        for text in texts:
+            for topic in self.parse(text.strip()):
+                if topic not in index:
+                    index[topic] = 1
+                else:
+                    index[topic] += 1
+        return { key: value for key, value in sorted(index.items(), key = lambda item: item[1], reverse = True) }
+
+
+    def parse(self, text):
+        parser = self.spacy(text)
+        topics = []
+
+        for chunk in parser.noun_chunks:
+            topic = []
+            for index, word in enumerate(chunk):
+                if (index > 0 or str(word) not in stop_words.STOP_WORDS) \
+                    and not word.is_punct \
+                    and word.pos_ not in ['PRON', 'ADJ', 'ADV', 'VERB', 'DET', 'CCONJ', 'SCONJ']:
+                    topic.append(str(word.lemma_).strip().lower())
+            if topic and len(topic) < 4:
+                topics.append(" ".join(topic))
+
+        return topics
