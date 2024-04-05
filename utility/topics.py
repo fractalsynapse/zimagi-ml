623cfa2223eddd00b19d1fa05bea0c9de09ed0c3
Adrian Webb <adrian@webb.sh>
2024-04-05 06:09:38 -0400

    Updating the topic modelling utility class to chunk text based on memory limit.

--- utility/topics.py
+++ utility/topics.py
@@ -6,9 +6,12 @@ import spacy
 
 class TopicModel(object):
 
+    text_max_length = 1000000
+
+
     def __init__(self):
         self.spacy = spacy.load('en_core_web_lg')
-        self.spacy.max_length = 10000000
+        self.spacy.max_length = self.text_max_length
 
 
     def filtered_index(self, full_texts, context_texts):
@@ -33,16 +36,32 @@ class TopicModel(object):
 
 
     def parse(self, text):
+        if len(text) >= self.text_max_length:
+            sentence_index = None
+            for match in re.finditer(r'([^\.]\.|\?|\!)(?=\s+)', text[:self.text_max_length], re.MULTILINE):
+                sentence_index = match.end()
+            sentence_index = sentence_index if sentence_index else self.text_max_length
+
+            topics = [
+                *self._parse(text[:sentence_index].strip()),
+                *self.parse(text[sentence_index:].strip())
+            ]
+        else:
+            topics = self._parse(text)
+
+        return topics
+
+    def _parse(self, text):
         parser = self.spacy(text)
         topics = []
 
         for chunk in parser.noun_chunks:
-            if not bool(re.search(r'([^\x00-\x7F]|\d+|\'|\"|\(|\)|\[|\]|\.)', str(chunk))):
+            if not bool(re.search(r'([^\x00-\x7F]|\d+|\'|\"|\?|\(|\)|\[|\]|\||\=|\.)', str(chunk))):
                 topic = []
                 for index, word in enumerate(chunk):
                     if (index > 0 or str(word) not in stop_words.STOP_WORDS) \
                         and not word.is_punct \
-                        and len(str(word)) > 1 \
+                        and len(str(word)) > 3 \
                         and word.pos_ not in ['PRON', 'ADP', 'ADJ', 'ADV', 'VERB', 'DET', 'CCONJ', 'SCONJ']:
                         topic.append(str(word.lemma_).strip().lower())
 
