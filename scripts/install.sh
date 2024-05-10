62f4ce3ca3313173501115479d337ae6651757ba
Adrian Webb <adrian@webb.sh>
2024-05-10 17:00:25 -0400

    Updating package requirement versions.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,6 @@ apt-get update
 apt-get install -y tesseract-ocr poppler-utils zlib1g-dev
 
 echo "Installing Pytorch and Spacy"
-python3 -m pip install "torch>=2.1.0"
-python3 -m pip install "spacy>=3.5.0"
+python3 -m pip install "torch>=2.3.0"
+python3 -m pip install "spacy>=3.7.4"
 python3 -m spacy download en_core_web_lg
