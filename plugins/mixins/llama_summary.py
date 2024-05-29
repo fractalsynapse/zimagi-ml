78c0821d4e0af24d0f97be0eebe08eba4bb2aeb4
Adrian Webb <adrian@webb.sh>
2024-05-28 21:58:52 -0400

    Updating the _get_prompt implementations in the Llama and Mixtral plugin mixins.

--- plugins/mixins/llama_summary.py
+++ plugins/mixins/llama_summary.py
@@ -4,6 +4,32 @@ from systems.plugins.index import ProviderMixin
 class LlamaSummaryMixin(ProviderMixin('llama_summary')):
 
     def _get_prompt(self, text, prompt = '', persona = '', output_format = ''):
+        if text.strip():
+            return self._get_text_prompt(text, prompt, persona, output_format)
+        return self._get_base_prompt(prompt, persona, output_format)
+
+
+    def _get_base_prompt(self, prompt, persona, output_format):
+        return """
+<s>[INST] <<SYS>>
+{persona}
+You always produce factually correct information from the text provided.
+You do not ask questions.
+<</SYS>>
+
+{instruction}
+
+Do not include external links.
+{output_format}
+[/INST]
+        """.format(
+            persona = persona.strip(),
+            instruction = prompt.strip(),
+            output_format = output_format.strip()
+        ).strip()
+
+
+    def _get_text_prompt(self, text, prompt, persona, output_format):
         return """
 <s>[INST] <<SYS>>
 {persona}
@@ -28,6 +54,7 @@ Do not include external links.
             text = text.strip(),
             output_format = output_format.strip()
         ).strip()
+
 
     def _parse_summary_response(self, summary):
         return summary.strip()
--- plugins/mixins/mixtral_summary.py
+++ plugins/mixins/mixtral_summary.py
@@ -4,6 +4,32 @@ from systems.plugins.index import ProviderMixin
 class MixtralSummaryMixin(ProviderMixin('mixtral_summary')):
 
     def _get_prompt(self, text, prompt = '', persona = '', output_format = ''):
+        if text.strip():
+            return self._get_text_prompt(text, prompt, persona, output_format)
+        return self._get_base_prompt(prompt, persona, output_format)
+
+
+    def _get_base_prompt(self, prompt, persona, output_format):
+        return """
+<s>[INST] <<SYS>>
+{persona}
+You always produce factually correct information from the text provided.
+You do not ask questions.
+<</SYS>>
+
+{instruction}
+
+Do not include external links.
+{output_format}
+[/INST]
+        """.format(
+            persona = persona.strip(),
+            instruction = prompt.strip(),
+            output_format = output_format.strip()
+        ).strip()
+
+
+    def _get_text_prompt(self, text, prompt, persona, output_format):
         return """
 <s>[INST] <<SYS>>
 {persona}
@@ -29,6 +55,7 @@ Do not include external links.
             text = text.strip(),
             output_format = output_format.strip()
         ).strip()
+
 
     def _parse_summary_response(self, summary):
         return summary.strip().strip("\'\"")
